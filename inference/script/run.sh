f7bc8c1cdabf09bfaf783d264f7f7d2e4b025b70
dablro12 <dablro1232@gmail.com>
2024-07-10 17:42:50 +0900

    Complete Project

--- inference/script/run.sh
+++ inference/script/run.sh
@@ -2,11 +2,9 @@
 
 # $1: model_name
 
-
-
-/home/eiden/anaconda3/envs/eiden/bin/python3 "/home/eiden/eiden/octc-cascade/inference/script/script.py" \
-    --img_dir 'web/DB/data' \
-    --save_dir 'web/DB/output' \
+/home/eiden/anaconda3/envs/chest-segmentation/bin/python3 "/home/eiden/eiden/octc-cascade/inference/script/script.py" \
+    --img_dir '/home/eiden/eiden/octc-cascade/web/DB/data' \
+    --save_dir '/home/eiden/eiden/octc-cascade/web/DB/output' \
     --segment_model_path '/mnt/HDD/oci-seg_models/monai_swinunet_v4_240530/model_400.pt' \
     --inpaint_model_path '/mnt/HDD/oci_models/aotgan/OCI-GAN_v3_240508/model_64.pt' \
     --batch_size 1 \
